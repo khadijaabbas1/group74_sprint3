1373ca247a627558fbc4e7b8a1e2eb1cbb7a8752
khadijaabbas1 <156485126+khadijaabbas1@users.noreply.github.com>
2025-04-03 21:24:45 -0400

    Update forms.py

--- cypress_project (dj files)/reports/forms.py
+++ cypress_project (dj files)/reports/forms.py
@@ -1,1 +1,7 @@
-forms
+from django import forms
+from .models import Report
+
+class ReportForm(forms.ModelForm):
+    class Meta:
+        model = Report
+        fields = ['issue_type', 'description', 'location', 'photo']
