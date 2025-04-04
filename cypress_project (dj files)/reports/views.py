981acbab2d956e10d50d40029dc33da832e889fd
khadijaabbas1 <156485126+khadijaabbas1@users.noreply.github.com>
2025-04-03 21:25:00 -0400

    Update views.py

--- cypress_project (dj files)/reports/views.py
+++ cypress_project (dj files)/reports/views.py
@@ -1,1 +1,12 @@
-views
+from django.shortcuts import render, redirect
+from .forms import ReportForm
+
+def submit_report(request):
+    if request.method == 'POST':
+        form = ReportForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'report_success.html')
+    else:
+        form = ReportForm()
+    return render(request, 'submit_report.html', {'form': form})
