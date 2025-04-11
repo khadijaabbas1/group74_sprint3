6019eff283aaf328fcbc2b98fd00bf188eff1113
khadijaabbas1 <156485126+khadijaabbas1@users.noreply.github.com>
2025-04-11 14:37:47 -0400

    Update forms.py

--- reports/forms.py
+++ reports/forms.py
@@ -4,4 +4,7 @@ from .models import Report
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ['issue_type', 'description', 'location', 'photo']
+        fields = '__all__'
+        widgets = {
+            'subscribed': forms.CheckboxInput(),
+        }
