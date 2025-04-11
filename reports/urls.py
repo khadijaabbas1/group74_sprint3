1670f4f9f7d5d7855735b3f2d95ef948db633df2
khadijaabbas1 <156485126+khadijaabbas1@users.noreply.github.com>
2025-04-11 14:41:03 -0400

    Update urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('submit/', views.submit_report, name='submit-report'),
+    path('submit/', views.submit_report, name='submit_report'),
+    path('api/reports/', views.all_reports, name='all_reports'),
 ]
